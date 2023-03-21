ef20bd6ac47a81a4e026cefd09a0ba2a98d1adec
Stephen <stephenlevesque2@gmail.com>
2023-03-20 18:38:12 -0700

    change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: '',
+      cityData: {},
+      error: false,
+      errorMessage: ''
+    }
+  }
+
+  handleCityInput = (event) => {
+    this.setState({
+      city: event.target.value
+    })
+  }
+
+  // ** async/await - handle our asynchronous code
+  // ** try/catch - handle our errors - TRY resolve our successful promises & CATCH handle rejected promise
+
+  getCityData = async (event) => {
+    event.preventDefault();
+
+    try {
+      // TODO: Use axios to get the data from LocationIQ - using city in state
+      let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
+
+      let cityDataFromAxios = await axios.get(url);
+
+      console.log(cityDataFromAxios.data[0])
+
+      // TODO: Set State with the data that comes back from axios & set error boolean to false
+      this.setState({
+        cityData: cityDataFromAxios.data[0],
+        error: false
+      });
+
+    } catch (error) {
+
+      // TODO: Set state with the error boolean and the error message
+      this.setState({
+        error: true,
+        errorMessage: error.message
+      })
+    }
+
+  }
+
+  // *** MAP PORTION OF YOUR LAB IMG SRC POINTS TO THIS URL: 
+  // *** https://maps.locationiq.com/v3/staticmap?key=<YOUR API KEY>&center=<CITY'S LAT>,<CITY'S LON>&zoom=13
+
+  render() {
+    return (
+      <>
+        <h1>API CALLS</h1>
+
+        <form onSubmit={this.getCityData}>
+          <label > Enter in a City:
+            <input type="text" onChange={this.handleCityInput} />
+          </label>
+          <button type="submit">Explore!</button>
+        </form>
+
+        {/* TERNARY - WTF  */}
+        {
+          this.state.error
+            ? <p>{this.state.errorMessage}</p>
+            : <p>{this.state.cityData.display_name}</p>
+        }
+
+
+      </>
+    )
+  }
 }
 
 export default App;
