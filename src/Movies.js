17d9e55611bcad24fe7c49060c719f006bb3a743
Stephen <stephenlevesque2@gmail.com>
2023-03-23 14:17:56 -0700

    corrections

--- src/Movies.js
+++ src/Movies.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import Movie from './Movie';
 
 class Movies extends React.Component {
   render() {
@@ -7,17 +8,7 @@ class Movies extends React.Component {
       <Carousel >
         {this.props.movieInfo.map((movie, idx) => {
           return (
-            <Carousel.Item key={idx}>
-              <img
-                className="d-block w-100"
-                src={`https://image.tmdb.org/t/p/w500/${movie.image}`}
-                alt={movie.title}
-              />
-              <Carousel.Caption className='carousel-caption'>
-                <h3>{movie.title}</h3>
-                <p className='carousel-text'>{movie.overview}</p>
-              </Carousel.Caption>
-            </Carousel.Item>
+         <Movie movie={movie} idx={idx}/>
           )
         })}
       </Carousel>
